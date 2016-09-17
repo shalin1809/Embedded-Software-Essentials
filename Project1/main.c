571b4afa5c0daed271d2166122968f7390871495
Shalin Shah <shalin.d.shah@colorado.edu>
2016-09-17 17:23:28 -0600

    Main file updated for working

--- Project1/main.c
+++ Project1/main.c
@@ -1,1 +1,28 @@
+/********************************************
+*   File: main.c
+*
+*   Copyrights 2016 Shalin Shah and Snehal Sanghvi
+*   All Rights Reserved
+*
+*
+*   The information contained herein is property of the Authors. 
+*   The copying and distribution of the files is prohibited except
+*   by express written agreement with the Authors.
+*
+*
+*   Authors: Shalin Shah and Snehal Sanghvi
+*   Date Edited: 17 Sept 2016
+*
+*   Description: Main file for project 1 library
+*
+*
+********************************************************/
 
+
+#include"project_1.h"
+#include"stdint.h"
+
+int8_t main(){
+    project_1_report();
+    return 0;
+}
