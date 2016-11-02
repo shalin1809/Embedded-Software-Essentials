e88d4f746c3cdfa718b6f9f01925975b51fde2ec
smsanghvi <snehal.sanghvi@colorado.edu>
2016-11-02 10:49:52 -0600

    Made changes to the SPI source file

--- Project3/Part-3/spi.c
+++ Project3/Part-3/spi.c
@@ -24,11 +24,15 @@ void SPI_init(void){
 	PORTC_PCR7 |= PORT_PCR_MUX(2); 			//Select Alternate function 2 for SPI0_MISO
 
 	//configuring the ss pin
-	PTC->PDDR |= 0x01;          //selecting the ss pin as output
-	PTC->PSOR = 0x01;						//making the ss pin high (or idle, since its active low)
+	PTC->PDDR |= 0x10;                      //selecting the ss pin as output (---- 1---)
+	PTC->PSOR = 0x10;						//making the ss pin high (or idle, since its active low)
 
 	//Selecting master mode for the SPI on MCU and setting CPHA to 1
 	SPI0->C1 |= SPI_C1_MSTR_MASK | SPI_C1_CPHA_MASK;
+
+	//enabling dma transfers
+	//SPI_C2 = SPI_C2_TXDMAE_MASK | SPI_C2_RXDMAE_MASK;
+	//SPI0->C2 = SPI_C2_SPC0_MASK | SPI_C2_BIDIROE_MASK;
 
 	//setting a baud rate divisor of 4
 	SPI0->BR = SPI_BR_SPR(1);
@@ -45,9 +49,9 @@ uint8_t SPI_status(void) {
 
 
 //transmitting a byte
-void SPI_tx_byte(unsigned char byte){
-	unsigned char temp;
-	PTC->PCOR |= 0x01;   //clearing the ss bit to drive it low
+uint8_t SPI_tx_byte(unsigned char byte){
+	uint8_t temp;
+	PTC->PCOR |= 0x10;   //clearing the ss bit to drive it low
 
 	//waiting for transmit buffer to become empty
 	while(!(SPI_status() & 0x20)){};
@@ -57,6 +61,25 @@ void SPI_tx_byte(unsigned char byte){
 	while(!(SPI_status() & 0x80)){};
 	temp = SPI0->D;   //clears SPRF
 
-	PTC->PSOR |= 0x01;	 //setting the ss bit high
+	PTC->PSOR |= 0x10;	 //setting the ss bit high
+	return temp;
 }
 
+
+//receiving a byte
+uint8_t SPI_rx_byte(){
+	uint8_t read_value;
+	PTC->PCOR |= 0x10;   //clearing the ss bit to drive it low
+
+	//waiting for transmit buffer to become empty
+	while(!(SPI_status() & 0x20)){};
+	SPI0_D = 0xFF;
+
+	//waiting for data to become available in the receive buffer
+	while(!(SPI_status() & 0x80)){};
+	read_value = SPI0->D;
+
+	PTC->PSOR |= 0x10;	 //setting the ss bit high
+	return read_value;
+
+}
