e0a388e0cf924657db00bf49d615a3a8678dfd63
smsanghvi <snehal.sanghvi@colorado.edu>
2016-12-01 18:41:55 -0700

    Updated main.c

--- Final-Project/main.c
+++ Final-Project/main.c
@@ -12,19 +12,24 @@
 *   Author: Snehal Sanghvi and Shalin Shah
 *   Date Edited: 26 Nov 2016
 *
-*   Description: main.c file for our final ESE project
+*   Description: Source file for our final ESE project
 
 ********************************************************/
 
 #include "MKL25Z4.h"
 #include "adc.h"
 #include "log.h"
+#include "dac.h"
 #include <stdint.h>
+
+uint16_t result_TEMP;
+uint16_t result_LDR;
+uint32_t average_result_TEMP=0;
+float voltage_TEMP = 0.0;
+float temperature = 0.0;
 
 int main (void)
 {
-uint16_t result;
-
 /* Configure ADC0 */
 ADC0_init();
 
@@ -32,20 +37,59 @@ ADC0_init();
 UART0_init();
 
 //Calibrating the adc module
-adc_cal();
+ADC0_calibrate();
+DAC0_init(); /* Configure DAC0 */
 
 while (1) {
-	adc_cal();
+	ADC0_calibrate();
 
-	ADC0->SC1[0] = 14; /* start conversion on channel 14 for LDR*/
+/************************************************************
+ * DAC buzzer
+ ***********************************************************/
+	int i,j;
+	for (i = 0; i < 0x1000; i += 0x8000) {
+	/* write value of i to DAC0 */
 
+	DAC0->DAT[0].DATL = 0xff; /* write low byte */
+	DAC0->DAT[0].DATH = 0x0f;
+	for(j=0;j<500;j++){
+
+	}
+	}
+
+
+/*************************************************************
+* ADC conversions for LDR
+*************************************************************/
+#ifdef LDR
+	ADC0->SC1[0] = 11; /* start conversion on channel 11 for LDR*/
 	/* wait for COCO */
 	while(!(ADC0->SC1[0] & 0x80)) { }
-
 	/* read conversion result and clear COCO flag */
-	result = ADC0->R[0];
+	result_LDR = ADC0->R[0];
+	//logging the result
+	LOG_1("\n\rADC value of LDR is :",23,(uint16_t)result_LDR,10);
+#endif
 
-	//Logging the result
-	LOG_1("\n\rADC value is :",16,(uint16_t)result,8);
+/*************************************************************
+* ADC conversions for Temperature sensor
+*************************************************************/
+#ifdef TEMP
+	ADC0->SC1[0] = 14; /* start conversion on channel 14 for temp sensor*/
+	/* wait for COCO */
+	while(!(ADC0->SC1[0] & 0x80)) { }
+	/* read conversion result and clear COCO flag */
+	result_TEMP = ADC0->R[0];
+	/*Taking an average */
+	average_result_TEMP = mean_filter(result_TEMP);
+
+	if(average_result_TEMP!=0){
+		voltage_TEMP = (average_result_TEMP/65536.0) * 3.3;
+		temperature = (voltage_TEMP - 0.5)/0.01;
+		//Logging the result
+		LOG_1("\n\rADC value of temp sensor is :",30,(uint16_t)average_result_TEMP,10);
+		LOG_2("\n\rTemperature (in Celsius) is :",30,temperature,10);
+	}
+#endif
  }
 }
