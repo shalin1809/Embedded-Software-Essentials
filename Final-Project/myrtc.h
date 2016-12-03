193dc9e7d29b219ddd0ca511168b20e30c40609c
Shalin Shah <shalin.d.shah@colorado.edu>
2016-12-03 12:26:22 -0700

    Implemented UNIX timestamp

--- Final-Project/myrtc.h
+++ Final-Project/myrtc.h
@@ -9,24 +9,60 @@
 *   by express written agreement with the Authors.
 *
 *   Authors: Shalin Shah and Snehal Sanghvi 
-*   Date Edited: 1 Dec 2016
+*   Date Edited: 3 Dec 2016
 *
 *   Description: Header file for using Real time clock on KL25Z
 *               -rtc_init
-*               -RTC_Seconds_IRQHandler
+*               -get_unix_time
+*               -calculate_date
+*               -get_rtc_seconds
+*               -get_rtc_minutes
+*               -get_rtc_hours
+*               -get_rtc_year
+*               -get_rtc_month
+*               -get_rtc_day
 *
 ********************************************************/
 
+#ifndef MYRTC_H_
+#define MYRTC_H_
+
 #include "MKL25Z4.h"
 #include "stdint.h"
+
+
 
 
 typedef struct myrtc_t {
     uint8_t seconds;
     uint8_t minutes;
     uint8_t hours;
+    uint16_t year;
+    uint8_t month;
+    uint8_t day;
 }rtc;
+
 
 void rtc_init(void) ;
 
-void RTC_Seconds_IRQHandler(void);
+void UART0_WriteString(char string[]);
+
+void UART0_WriteChar(char BYTE);
+
+uint32_t get_unix_time(void);
+
+void calculate_date(void);
+
+uint8_t get_rtc_seconds(void);
+
+uint8_t get_rtc_minutes(void);
+
+uint8_t get_rtc_hours(void);
+
+uint16_t get_rtc_year(void);
+
+uint8_t get_rtc_month(void);
+
+uint8_t get_rtc_day(void);
+
+#endif
